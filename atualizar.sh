40c84403706abfae12d27bab48d366c082f1bee4
carmon-byts <aldamiguelrafael@gmail.com>
2025-05-04 20:27:43 +0200

    Atualização do site

--- atualizar.sh
+++ atualizar.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Navegar até o diretório do projeto (opcional, caso esteja em outro diretório)
+# Navegar até o diretório do projeto
 cd ~/moz-vip-way-atendimento
 
-# Inicializar o repositório Git (caso não tenha feito isso antes)
-git init
-
-# Adicionar o repositório remoto (substitua pelo seu repositório GitHub)
-git remote add origin https://github.com/carmon-byts/seu-repositorio.git
-
-# Adicionar todas as mudanças no repositório
+# Adicionar as alterações
 git add .
 
 # Fazer o commit com uma mensagem
 git commit -m "Atualização do site"
 
-# Enviar as alterações para o GitHub
-git push -u origin master
+# Enviar as alterações para a branch principal (main)
+git push -u origin main
